195cfae4bbd0a10bcedc7261c24ab71bbd588a34
eeunhyee <eunhyee657@gmail.com>
2025-08-22 18:44:06 +0900

    chore: Add Initial Setup

---
+++ monty_hall.py
@@ -0,0 +1,7 @@
+# 은혜님 – 환경 설정 및 파라미터 정의 담당
+from random import randrange
+
+doors, iterations = 3, 100000  # could try 100,1000
+
+print("\nMonty Hall problem simulation:")
+print(f"{doors} doors, {iterations} iterations.\n")
