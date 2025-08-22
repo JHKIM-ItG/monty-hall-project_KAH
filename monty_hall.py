6f141c185bdb275b89d039a4b781d95975d916a3
JHKIM-ItG <past7080@gmail.com>
2025-08-22 18:41:38 +0900

    test : test code

--- monty_hall.py
+++ monty_hall.py
@@ -1,21 +1,5 @@
-from random import randrange
-
-doors, iterations = 3,100000 # could try 100,1000
-def monty_hall(choice, switch=False, doorCount=doors):
-        # Set up doors
-        door = [False]*doorCount
-        # One door with prize
-        door[randrange(doorCount)] = True
-        chosen = door[choice]
-        unpicked = door
-        del unpicked[choice]
-        # Out of those unpicked, the alternative is either:
-        # the prize door, or
-        # an empty door if the initial choice is actually the prize.
-        alternative = True in unpicked
-        if switch:
-                return alternative
-        else:
-                return chosen
-        print ("Monty Hall problem simulation:")
-        print (doors, "doors,", iterations, "iterations.")
+# 종화님 – 결과 출력 및 해석 담당
+print(f"Not switching allows you to win {not_switching_wins} out of {iterations} times.")
+print(f"Switching allows you to win {switching_wins} out of {iterations} times.\n")
+print(f"Winning rate without switching: {not_switching_wins / iterations:.2%}")
+print(f"Winning rate with switching: {switching_wins / iterations:.2%}")
